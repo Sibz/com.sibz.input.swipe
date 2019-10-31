baa332517a2c1288afba2f5a1e606c78e5bef52e
Steven Turner <steven@entityzero.com>
2019-10-31 21:39:59 +1100

    Initial Commit

---
+++ Runtime/SwipeComposite.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.Utilities;
+
+
+namespace Sibz.Input.Swipe
+{
+
+#if UNITY_EDITOR
+    [InitializeOnLoad] // Automatically register in editor.
+#endif
+    public class SwipeComposite : InputBindingComposite<Vector2>
+    {
+
+        [InputControl(layout = "Vector2")]
+        public int Delta;
+
+        [InputControl(layout = "Button")]
+        public int Trigger;
+
+        public float MinDistance = 10f;
+
+        public enum ValueType
+        {
+            Delta,
+            NormalisedDelta,
+            OneUnitInt
+        }
+        public ValueType ReturnValueType = ValueType.Delta;
+
+        private bool modifierActive = false;
+        private Vector2 totalDelta;
+
+
+        public override Vector2 ReadValue(ref InputBindingCompositeContext context)
+        {
+            totalDelta += context.ReadValue<Vector2, Vector2MagnitudeComparer>(Delta);
+
+            if (Mathf.Abs(totalDelta.x) < MinDistance && Mathf.Abs(totalDelta.y) < MinDistance)
+            {
+                return Vector2.zero;
+            }
+
+            switch (ReturnValueType)
+            {
+                case ValueType.Delta:
+                    return totalDelta;
+                case ValueType.OneUnitInt:
+                    return new Vector2(totalDelta.x > 0 ? 1 : -1, totalDelta.y > 0 ? 1 : -1);
+                case ValueType.NormalisedDelta:
+                default:
+                    return totalDelta.normalized;
+            }
+        }
+
+        static SwipeComposite()
+        {
+            InputSystem.RegisterBindingComposite<SwipeComposite>();
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Init() { } // Trigger static constructor.
+
+        public override float EvaluateMagnitude(ref InputBindingCompositeContext context)
+        {
+            var modifierCurrentlyActive = context.ReadValueAsButton(Trigger);
+            if (modifierActive && !modifierCurrentlyActive)
+            {
+                modifierActive = false;
+            }
+            else if (!modifierActive && modifierCurrentlyActive)
+            {
+                modifierActive = true;
+                totalDelta = Vector2.zero;
+            }
+
+            if (modifierActive)
+            {
+                var val = ReadValue(ref context);
+                if (val.x != 0 || val.y != 0)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Runtime/SwipeInteraction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Sibz.Input.Swipe
+{
+
+#if UNITY_EDITOR
+    [InitializeOnLoad] // Automatically register in editor.
+#endif
+    public class SwipeInteraction : IInputInteraction
+    {
+
+        public float minDuration = 0.2f;
+        public float maxDuration = 0.75f;
+
+        private float startTime;
+
+        public void Process(ref InputInteractionContext context)
+        {
+            if (context.isWaiting && context.ControlIsActuated())
+            {
+                context.Started();
+                startTime = Time.time;
+            }
+            else if (context.isStarted && !context.ControlIsActuated())
+            {
+                var duration = Time.time - startTime;
+                if (duration > minDuration &&
+                    duration < maxDuration)
+                {
+                    // Interaction has been completed.
+                    context.Performed();
+                }
+                else
+                {
+                    context.Canceled();
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            startTime = 0;
+        }
+
+        static SwipeInteraction()
+        {
+            InputSystem.RegisterInteraction<SwipeInteraction>();
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Init() { } // Trigger static constructor.
+
+    }
+}
